6ec26fa12fb08b0ebf6711f71cf0d00975cd53bb
Ben <ben.gu.au@gmail.com>
2019-05-13 15:28:08 +1000

    Add handleLike event handler

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -13,7 +13,11 @@ class Movies extends Component {
   };
 
   handleLike = movie => {
-    console.log("Like clicked:", movie);
+    const movies = [...this.state.movies];
+    const index = movies.indexOf(movie);
+    movies[index] = { ...movies[index] };
+    movies[index].liked = !movies[index].liked;
+    this.setState({ movies });
   };
 
   render() {
