ffcd74230ad99d52e3760efe9f55b43142a959f2
Ben <ben.gu.au@gmail.com>
2019-05-13 14:42:39 +1000

    Create like component using font Awesome - heart

---
+++ src/components/common/like.jsx
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+
+// Input:  liked: boolean
+// Output: onClick
+
+class Like extends Component {
+  state = {};
+  render() {
+    let classes = "fa fa-heart";
+    if (!this.props.liked) classes += "-o";
+    return (
+      <i
+        onClick={this.props.onClick}
+        style={{ cursor: "pointer" }}
+        className={classes}
+        aria-hidden="true"
+      />
+    );
+  }
+}
+// Input:  liked: boolean
+// Output: onClick
+
+export default Like;
