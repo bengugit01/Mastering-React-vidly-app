b3a356f233ce5a25aa8c247094c6daff4c8c3693
Ben <ben.gu.au@gmail.com>
2019-05-17 12:08:26 +1000

    Refactoring:  Destructuring Arguments

--- src/components/common/tableBody.jsx
+++ src/components/common/tableBody.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import _ from "lodash";
+
 class TableBody extends Component {
   renderCell = (item, column) => {
     if (column.content) return column.content(item);
@@ -7,19 +8,20 @@ class TableBody extends Component {
     return _.get(item, column.path);
   };
 
-  createKey(item, column) {
+  createKey = (item, column) => {
     return item._id + (column.path || column.key);
-  }
+  };
 
   render() {
     const { data, columns } = this.props;
+
     return (
       <tbody>
         {data.map(item => (
           <tr key={item._id}>
             {columns.map(column => (
               <td key={this.createKey(item, column)}>
-                {this.renderCell(item, column)}{" "}
+                {this.renderCell(item, column)}
               </td>
             ))}
           </tr>
