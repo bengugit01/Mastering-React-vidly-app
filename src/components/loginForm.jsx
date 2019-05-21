39281b115c869c637ca61d708f19f104202ad347
Ben <ben.gu.au@gmail.com>
2019-05-21 20:50:14 +1000

    Creating React Ref Object to access to the DOM. e.g. set focus().

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -1,12 +1,20 @@
 import React, { Component } from "react";
 
 class LoginForm extends Component {
+  username = React.createRef();
+
+  componentDidMount() {
+    this.username.current.focus();
+  }
+
   handleSubmit = e => {
     e.preventDefault();
 
     // call the server
-    console.log("Submitted");
+    const username = this.username.current.value;
+    console.log("Submitted with username: ", username);
   };
+
   render() {
     return (
       <div>
@@ -14,7 +22,12 @@ class LoginForm extends Component {
         <form onSubmit={this.handleSubmit}>
           <div className="form-group">
             <label htmlFor="username">Username</label>
-            <input id="username" type="text" className="form-control" />
+            <input
+              ref={this.username}
+              id="username"
+              type="text"
+              className="form-control"
+            />
           </div>
           <div className="form-group">
             <label htmlFor="password">Password</label>
