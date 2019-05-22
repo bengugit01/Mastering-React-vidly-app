30f3c89fde08d2def613af61bcf81aebbe8a8dff
Ben <ben.gu.au@gmail.com>
2019-05-22 15:34:16 +1000

    Displaying validation errors.

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -24,8 +24,7 @@ class LoginForm extends Component {
     e.preventDefault();
 
     const errors = this.validate();
-    console.log(errors);
-    this.setState({ errors });
+    this.setState({ errors: errors || {} });
     if (errors) return;
 
     // call the server
@@ -39,7 +38,7 @@ class LoginForm extends Component {
   };
 
   render() {
-    const { account } = this.state;
+    const { account, errors } = this.state;
     return (
       <div>
         <h1>Login</h1>
@@ -49,12 +48,14 @@ class LoginForm extends Component {
             value={account.name}
             label="Username"
             onChange={this.handleChange}
+            error={errors.username}
           />
           <Input
             name="password"
             value={account.password}
             label="Password"
             onChange={this.handleChange}
+            error={errors.password}
           />
           <button className="btn btn-primary">Login</button>
         </form>
