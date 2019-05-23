e01d9899927f5c240dc35485f812658f201b12ce
Ben <ben.gu.au@gmail.com>
2019-05-23 13:52:33 +1000

    Extracting helper rendering methods, and also using the rest and spread operator.

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import Joi from "joi-browser";
-import Form form './common/form';
-import Input from "./common/input";
+import Form from "./common/form";
 
 class LoginForm extends Form {
   state = {
@@ -24,28 +23,13 @@ class LoginForm extends Form {
   };
 
   render() {
-    const { data, errors } = this.state;
     return (
       <div>
         <h1>Login</h1>
         <form onSubmit={this.handleSubmit}>
-          <Input
-            name="username"
-            value={data.name}
-            label="Username"
-            onChange={this.handleChange}
-            error={errors.username}
-          />
-          <Input
-            name="password"
-            value={data.password}
-            label="Password"
-            onChange={this.handleChange}
-            error={errors.password}
-          />
-          <button disabled={this.validate()} className="btn btn-primary">
-            Login
-          </button>
+          {this.renderInput("username", "Username")}
+          {this.renderInput("password", "Password", "password")}
+          {this.renderButton("Login")}
         </form>
       </div>
     );
